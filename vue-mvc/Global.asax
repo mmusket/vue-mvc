f190af81db22e888c2b0ef5734ab55a5c7e6ea31
Marco Muscat <marco.muscat@gmail.com>
2016-08-08 23:14:18 +0100

    Init

---
+++ vue-mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vue_mvc.MvcApplication" Language="C#" %>
---
+++ vue-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vue_mvc.Startup))]
+namespace vue_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
